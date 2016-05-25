80dcfec09a30e44203fe3a925f2e282a7f0605f4
Sam Lambert <sam.a.lambert@gmail.com>
2016-05-25 16:51:09 -0400

    _generate.sh back to original

--- src/_seq/_generate.sh
+++ src/_seq/_generate.sh
@@ -1,1 +1,1 @@
-awk 'BEGIN { for(i=0;i<200000;i++){ for(j=0;j<200;j++) { n=rand(); if(n<0.25) printf("A"); else if(n<0.5) printf("C"); else if(n<0.75) printf("G"); else printf("T"); } printf("\n"); } }'
+awk 'BEGIN { for(i=0;i<200000;i++){ for(j=0;j<200;j++) { n=rand(); if(n<0.25) printf("A"); else if(n<0.5) printf("C"); else if(n<0.75) printf("G"); else printf("T"); } printf("\n"); } }' > sequences.random.txt
